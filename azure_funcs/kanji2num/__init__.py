5cfa9a98db394a2e17f0e4f42746c4e7085b8e92
I-Root-Route <SuganoShota1999@gmail.com>
2021-02-07 20:06:09 +0900

    created class

--- azure_funcs/kanji2num/__init__.py
+++ azure_funcs/kanji2num/__init__.py
@@ -7,85 +7,86 @@ import azure.functions as func
 from settings import settings
 
 
-def check_kanji(kanji_num: str) -> bool:
-    # 大字表記のはじめは必ずsettings.baseにあるべき。
-    # 例) 拾は認めず、壱拾は認める
-    if kanji_num[0] not in settings.base:
-        return False
-
-    temp = None
-    for i, kanji in enumerate(kanji_num):
-        if temp is None:
-            temp = kanji
-            continue
-        # 壱参などの形は認めない
-        if temp[-1] in settings.base and kanji in settings.base:
+class K2N(object):
+    @staticmethod
+    def check_kanji(kanji_num: str) -> bool:
+        # 大字表記のはじめは必ずsettings.baseにあるべき。
+        # 例) 拾は認めず、壱拾は認める
+        if kanji_num[0] not in settings.base:
             return False
-        else:
-            temp += kanji
-    return True
 
+        temp = None
+        for i, kanji in enumerate(kanji_num):
+            if temp is None:
+                temp = kanji
+                continue
+            # 壱参などの形は認めない
+            if temp[-1] in settings.base and kanji in settings.base:
+                return False
+            else:
+                temp += kanji
+        return True
 
-def split_to_chunks(kanji_num: str, temp='', overall=None, max_exp=None) -> List[Tuple[str]]:
-    # [("四百参拾弐", "兆"), ("四千参百弐拾七", "億").....]
-    # のような四つ区切りで出てくる位(万、億、兆)とそれ以外で分ける。
-    if overall is None:
-        overall = []
+    def split_to_chunks(self, kanji_num: str, temp='', overall=None, max_exp=None) -> List[Tuple[str]]:
+        # [("四百参拾弐", "兆"), ("四千参百弐拾七", "億").....]
+        # のような四つ区切りで出てくる位(万、億、兆)とそれ以外で分ける。
+        if overall is None:
+            overall = []
 
-    if max_exp is None:
-        max_exp = float('inf')
+        if max_exp is None:
+            max_exp = float('inf')
 
-    if not kanji_num:
-        return overall
+        if not kanji_num:
+            return overall
 
-    for i, kanji in enumerate(kanji_num):
-        if kanji not in settings.exp_base:
-            temp += kanji
-            if i == len(kanji_num) - 1:
-                overall.append((temp if temp else None, None))
-                return overall
-        else:
-            assert max_exp > settings.num_table[kanji]  # 壱兆参千「万」七「兆」のような形を受け付けない
-            overall.append((temp if temp else None, kanji))
-            return split_to_chunks(kanji_num[i + 1:], temp='', overall=overall, max_exp=settings.num_table[kanji])
+        for i, kanji in enumerate(kanji_num):
+            if kanji not in settings.exp_base:
+                temp += kanji
+                if i == len(kanji_num) - 1:
+                    overall.append((temp if temp else None, None))
+                    return overall
+            else:
+                assert max_exp > settings.num_table[kanji]  # 壱兆参千「万」七「兆」のような形を受け付けない
+                overall.append((temp if temp else None, kanji))
+                return self.split_to_chunks(kanji_num[i + 1:], temp='', overall=overall,
+                                            max_exp=settings.num_table[kanji])
 
-
-def kanji_to_num_helper(base: str) -> int:
-    # チャンクごとに漢数字をアラビア数字に変換する
-    base_num = 0
-    temp = 0
-    max_chunk_base = 0
-    for i, kanji in enumerate(base):
-        if kanji not in settings.exp:
-            temp += settings.num_table[kanji]
-            if i == len(base) - 1:
+    @staticmethod
+    def kanji_to_num_helper(base: str) -> int:
+        # チャンクごとに漢数字をアラビア数字に変換する
+        base_num = 0
+        temp = 0
+        max_chunk_base = 0
+        for i, kanji in enumerate(base):
+            if kanji not in settings.exp:
+                temp += settings.num_table[kanji]
+                if i == len(base) - 1:
+                    base_num += temp
+                    return base_num
+            else:
+                char_num = settings.num_table[kanji]
+                if char_num in settings.chunk_base:
+                    temp_max = max_chunk_base
+                    max_chunk_base = max(max_chunk_base, char_num)
+                temp *= char_num
                 base_num += temp
-                return base_num
-        else:
-            char_num = settings.num_table[kanji]
-            if char_num in settings.chunk_base:
-                temp_max = max_chunk_base
-                max_chunk_base = max(max_chunk_base, char_num)
-            temp *= char_num
-            base_num += temp
-            temp = 0
+                temp = 0
 
-    return base_num
+        return base_num
 
+    def kanji_to_num(self, kanji_num: str) -> int:
+        if not self.check_kanji(kanji_num):
+            raise ValueError
 
-def kanji_to_num(kanji_num: str) -> int:
-    if not check_kanji(kanji_num):
-        raise ValueError
+        num = 0
+        chunks = self.split_to_chunks(kanji_num)
+        for chunk in chunks:
+            base = chunk[0]
+            exp_base_num = settings.num_table[chunk[1]] if chunk[1] is not None else 1
+            base_num = self.kanji_to_num_helper(base)
+            num += base_num * exp_base_num
 
-    num = 0
-    chunks = split_to_chunks(kanji_num)
-    for chunk in chunks:
-        base = chunk[0]
-        exp_base_num = settings.num_table[chunk[1]] if chunk[1] is not None else 1
-        base_num = kanji_to_num_helper(base)
-        num += base_num * exp_base_num
-
-    return num
+        return num
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
@@ -94,8 +95,9 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     message = None
 
     try:
+        k2n = K2N()
         kanji = req.route_params.get('kanji')
-        num = kanji_to_num(kanji)
+        num = k2n.kanji_to_num(kanji)
         message = str(num)
     except ValueError:
         code = 204
--- azure_funcs/num2kanji/__init__.py
+++ azure_funcs/num2kanji/__init__.py
@@ -7,38 +7,39 @@ import azure.functions as func
 from settings import settings
 
 
-def split_to_chunks(number: int) -> List[str]:
-    # 上から4つづつの塊に分ける
-    chunks = []
-    for i in range(0, len(str(number)), 4):
-        temp = str(number)[::-1][i:i + 4]
-        chunks.append(temp[::-1])
+class N2K(object):
+    @staticmethod
+    def split_to_chunks(number: int) -> List[str]:
+        # 上から4つづつの塊に分ける
+        chunks = []
+        for i in range(0, len(str(number)), 4):
+            temp = str(number)[::-1][i:i + 4]
+            chunks.append(temp[::-1])
 
-    return chunks[::-1]
+        return chunks[::-1]
 
+    @staticmethod
+    def num_to_kanji_helper(chunk: str) -> str:
+        kanji_chunk = ''
+        for i, num in enumerate(chunk):
+            if int(num) == 0:
+                continue
+            rank = settings.chunk_base[len(chunk) - i - 1]
+            kanji_chunk += settings.kanjitable[int(num)] + rank
 
-def num_to_kanji_helper(chunk: List[str]) -> str:
-    kanji_chunk = ''
-    for i, num in enumerate(chunk):
-        if int(num) == 0:
-            continue
-        rank = settings.chunk_base[len(chunk) - i - 1]
-        kanji_chunk += settings.kanjitable[int(num)] + rank
+        return kanji_chunk
 
-    return kanji_chunk
+    def num_to_kanji(self, num: int) -> str:
+        assert 0 <= num <= 9999999999999999
+        if 0 <= num <= 10:
+            return settings.kanjitable[num]
+        out = ""
+        chunks = self.split_to_chunks(num)
+        for i, chunk in enumerate(chunks):
+            kanji_chunk = self.num_to_kanji_helper(chunk)
+            out += kanji_chunk + settings.split_base[len(chunks) - i - 1] if kanji_chunk else ''
 
-
-def num_to_kanji(num: int) -> str:
-    assert 0 <= num <= 9999999999999999
-    if 0 <= num <= 10:
-        return settings.kanjitable[num]
-    out = ""
-    chunks = split_to_chunks(num)
-    for i, chunk in enumerate(chunks):
-        kanji_chunk = num_to_kanji_helper(chunk)
-        out += kanji_chunk + settings.split_base[len(chunks) - i - 1] if kanji_chunk else ''
-
-    return out
+        return out
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
@@ -47,8 +48,9 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     message = None
 
     try:
+        n2k = N2K()
         num = int(req.route_params.get('num'))
-        kanji_num = num_to_kanji(num)
+        kanji_num = n2k.num_to_kanji(num)
         message = kanji_num
     except ValueError:
         code = 204
